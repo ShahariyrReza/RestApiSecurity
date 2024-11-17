af3ec45e5334c6eed22c4ed03bb34687e55de39e
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-11-17 23:06:12 +0600

    update configaration

--- src/main/java/com/shahariyr/springboot/cruddemo/security/SecurityConfig.java
+++ src/main/java/com/shahariyr/springboot/cruddemo/security/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.shahariyr.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -15,21 +19,58 @@ public class SecurityConfig {
 		UserDetails shahariyr = User.builder()
 				.username("shahariyr")
 				.password("{noop}1nc0rr3c7")
-				.roles("employee")
+				.roles("EMPLOYEE")
 				.build();
 		
 		UserDetails mazher = User.builder()
 				.username("mazher")
 				.password("{noop}1nc0rr3c7")
-				.roles("employee","manager")
+				.roles("EMPLOYEE","MANAGER")
 				.build();
 		
 		UserDetails maksud = User.builder()
 				.username("maksud")
 				.password("{noop}1nc0rr3c7")
-				.roles("employee","maager","CEO")
+				.roles("EMPLOYEE","MANAGER","ADMIN")
 				.build();
 		return new InMemoryUserDetailsManager(shahariyr,mazher,maksud);
  	}
+	
+	
+	// For role based authentication to get or modify database.
+	@Bean
+	SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+		
+		http.authorizeHttpRequests(configurer -> configurer
+				.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+				
+				);
+		//http basic authentication;
+		http.httpBasic(Customizer.withDefaults());
+		
+		
+		// Disable CSRF because it not necessary for PUT.POST,DELETE and/or PATCH;	
+		http.csrf(csrf->csrf
+				.disable());
+		
+		
+		return http.build();
+		
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
